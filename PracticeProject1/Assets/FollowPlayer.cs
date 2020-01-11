71cdedbbf1f0f85535fb2dd77a51947140a2bf66
Guang Zhang <guangkzh@gmail.com>
2020-01-11 18:12:40 -0500

    PracticeUnity1

---
+++ PracticeProject1/Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      transform.position = player.position + offset;
+    }
+}
